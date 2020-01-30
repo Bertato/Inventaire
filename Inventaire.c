8a60caf7a31b6f67118ee5bbe8bac79bc7fbaccb
bertato <benjamin.bertato@orange.fr>
2020-01-30 17:00:56 +0100

    avancer

--- Inventaire.c
+++ Inventaire.c
@@ -1,20 +1,33 @@
 #include <stdio.h>
 #include <string.h>
 
+retenir = 0;
 
 int main()
-{
-    char str1[] = "epee", str2[] = "potion", str3[] = "arc";
+{	
+	char obj1[] = "epee", obj2[] = "potion", obj3[] = "arc";
     int trier;
+	int i = 0;
     
-    trier = strcmp(str1, str2);
-    printf("strcmp(str1, str2) = %d\n", trier);
-   
-    trier = strcmp(str1, str3);
-    printf("strcmp(str1, str3) = %d\n", trier);
+	for (i=0;i>3;i++){
+		
+		
+		trier = strcmp(obj1, obj2);
+		printf("strcmp(obj1, obj2) = %d\n", trier);
+		
+		if (strcmp(obj1, obj2) == 1){
+			// strcpy permet de copier une chaine de caractere 
+			// retenir la premiere la copier dans l'autre et refaire le retenu dans le premier
+		
+	   
+		trier = strcmp(obj1, obj3);
+		printf("strcmp(obj1, obj3) = %d\n", trier);
+
+		
+		trier = strcmp(obj2, obj3);
+		printf("strcmp(obj2, obj3) = %d\n", trier);
 	
-	trier = strcmp(str2, str3);
-    printf("strcmp(str2, str3) = %d\n", trier);
+	}
 	
 	
     return 0;
